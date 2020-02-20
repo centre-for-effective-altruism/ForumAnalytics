813ceee0a0b34980f3638c6045d9397cb0050ac3
JP Addison <johnpaddison@gmail.com>
2020-02-20 12:19:18 -0800

    running queries on the db

---
+++ elephantsandpandas.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import psycopg2
+
+from utils import get_config_field
+
+conn = None
+
+
+def db_connect():
+    print("Opening new db connection")
+    global conn
+    account = get_config_field('POSTGRESDB', 'pg_account')
+    passwd = get_config_field('POSTGRESDB', 'pg_password')
+    host = get_config_field('POSTGRESDB', 'pg_host')
+    db_name = get_config_field('POSTGRESDB', 'pg_db_name')
+
+    conn = psycopg2.connect(f"host='{host}' dbname='{db_name}' user={account} password={passwd}")
+
+
+def db_query(sql):
+    if not conn:
+        db_connect()
+    data = pd.read_sql_query(sql, conn)
+    return data
