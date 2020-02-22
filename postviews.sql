5839da78df40447949877143e22831f4d60ec021
JP Addison <johnpaddison@gmail.com>
2020-02-21 16:24:57 -0800

    add age of viewer criterion

--- postviews.sql
+++ postviews.sql
@@ -8,17 +8,33 @@ WITH lessraw AS (
     (event ->> 'seconds')::NUMERIC AS seconds,
     event ->> 'increment' AS increment
   FROM raw
-    WHERE event_type = 'timerEvent'
+  -- timerEvent means you've been on active on a page for N seconds, recorded
+  -- with a backoff
+  WHERE event_type = 'timerEvent'
+), all_views AS (
+  SELECT
+    -- client_id is unique to each browser
+    client_id,
+    path,
+    -- TODO: Maybe get post_id here?
+    -- substring(path from 8 for N)
+    min(timestamp) AS first_viewed
+  FROM lessraw
+  WHERE substring(path from 2 for 5) = 'posts' AND
+    -- TODO: Seconds as input variable?
+    seconds >= 60 AND
+    environment = 'production'
+  GROUP BY path, client_id
+), first_visits AS (
+  SELECT
+    min(timestamp) AS timestamp,
+    client_id
+  FROM lessraw
+  GROUP BY client_id
 ) SELECT
-  -- client_id is unique to each browser
-  client_id,
-  path,
-  -- TODO: Maybe get post_id here?
-  -- substring(path from 8 for N)
-  min(timestamp) AS first_viewed
-FROM lessraw
-WHERE substring(path from 2 for 5) = 'posts' AND
- -- TODO: Seconds as input variable?
-  seconds >= 60 AND
-  environment = 'production'
-GROUP BY path, client_id;
+  all_views.*
+FROM all_views
+JOIN first_visits
+  ON all_views.client_id = first_visits.client_id
+WHERE
+  first_visits.timestamp < all_views.first_viewed - INTERVAL '3 days';
